822ead0b766ae437f3b4ecf0d7f3a9b2eda18de9
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2022-12-28 12:42:03 +0100

    add hover effect to checkbox

--- src/ui/Checkbox.jsx
+++ src/ui/Checkbox.jsx
@@ -1,7 +1,7 @@
 import { useId } from 'react'
 import { BsCheck } from 'react-icons/bs'
 
-const defaultStateCSS = 'h-6 w-6 rounded-full border border-gray-600 block cursor-pointer'
+const defaultStateCSS = 'h-6 w-6 rounded-full border border-gray-600 block cursor-pointer hover:border-2 hover:border-gray-500'
 const checkedStateCSS = `${defaultStateCSS} bg-gradient-to-r from-blue-400 to-indigo-500 flex items-center justify-center`
 
 export const Checkbox = ({ isChecked, onClick }) => {
